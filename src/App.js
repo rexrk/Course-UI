3d37529c2aadea2d796a875fc2bfcd4ce2d03c37
Raman Kumar <kraman938@gmail.com>
2024-08-26 21:15:00 +0530

    frontend completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AddCourse from './components/AddCourse';
+import ErrorComponent from './components/ErrorComponent'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AddCourse />} />
+        <Route path="/courses" element={<AddCourse />} />
+        <Route path='*' element={<ErrorComponent />} />
+      </Routes>
+    </Router>
   );
 }
 
